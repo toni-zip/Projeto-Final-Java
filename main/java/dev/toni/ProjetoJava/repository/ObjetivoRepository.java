03c5edbc285b1e2997794142cce2c798af46f8e5
Antonio Pereira <antoniofranciscolacerda@hotmail.com>
2025-05-26 00:25:41 -0300

    Delete main directory

--- main/java/dev/toni/ProjetoJava/repository/ObjetivoRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.toni.ProjetoJava.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.toni.ProjetoJava.model.Objetivo;
-
-public interface ObjetivoRepository extends JpaRepository<Objetivo, Long> {
-}
