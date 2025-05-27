9f0573d63125b307b7e8e07d500e46929044bde0
Antonio Pereira <antoniofranciscolacerda@hotmail.com>
2025-05-27 12:49:08 -0300

    Delete ProjetoJava/ProjetoJava/ProjetoJava directory

--- ProjetoJava/ProjetoJava/ProjetoJava/src/main/java/dev/toni/ProjetoJava/repository/ResultadoChaveRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.toni.ProjetoJava.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.toni.ProjetoJava.model.ResultadoChave;
-
-public interface ResultadoChaveRepository extends JpaRepository<ResultadoChave, Long> {
-}
